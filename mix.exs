46d75fa252ca59ba4f28c7e9d8987b93114c9cf9
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-06-27 16:30:04 +1000

    Update mix deps

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule LandingPage.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:httpoison, "~> 0.13"}
+      {:httpoison, "~> 1.0"}
     ]
   end
 
